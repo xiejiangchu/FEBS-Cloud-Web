4e368a95415590040725c63a2c8f0f0610a6f5db
mrbird <wuyouzhuguli@gmail.com>
2019-09-08 14:18:51 +0800

    init

---
+++ src/settings.js
@@ -0,0 +1,8 @@
+module.exports = {
+  title: 'FEBS Cloud',
+  springBootAdminUrl: 'http://localhost:8401',
+  swaggerUrl: 'http://localhost:8301/system/swagger-ui.html',
+  registerUrl: 'http://localhost:8001/register',
+  zipkinUrl: 'http://localhost:8402',
+  kibanaUrl: 'http://192.168.33.10:5601'
+}
